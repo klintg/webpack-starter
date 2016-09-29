03ada51f1a43d2546be8e8c3cd6e3a58f7a3762a
klintg <theklint1@gmail.com>
2016-09-29 15:06:21 +0300

    finalizing & production setup

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,57 @@
+/*container component */
+
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseAction';
+import {bindActionCreators} from 'redux';
+import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
+
+class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
+  render() {
+    const {courses} = this.props;
+
+    return (
+      <div>
+        <h1>Course</h1>
+      <input type="submit"
+        value="Add Course"
+        className="btn btn-primary"
+        onClick={this.redirectToAddCoursePage} />
+        <CourseList courses={courses} />
+      </div>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+//REDUX CONNECT AND RELATED FUNCTIONS.
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
